5d3de305225d86de901b499fb28775515c6dd40b
austinbhan <austinbhan@outlook.com>
2022-06-07 16:53:45 -0700

    added animal-card

---
+++ src/components/Animals/animalCard.js
@@ -0,0 +1,13 @@
+import './animalCard.css';
+
+export default function animalCard(name, catchphrase) {
+  return (
+
+    <div className = 'animal-card'>
+        <img src = {`/public/animals/${name}.svg`}></img>
+        <p>{name}</p>
+        <p>{catchphrase}</p>
+    </div>
+
+  );
+}
