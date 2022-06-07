892ca829024db77f08c6ebb93e676d5f759ca53f
austinbhan <austinbhan@outlook.com>
2022-06-07 16:36:08 -0700

    Created animals array

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -1,3 +1,14 @@
 export const animals = [
-    
-]
+  { name: 'Wilbur the Pig', 
+    catchphrase: 'You have been my friend. That in itself is a tremendous thing.' },
+  { name: 'Samuel the Sheep', 
+    catchphrase: `Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.` },
+  { name: 'Noel the Donkey', 
+    catchphrase: 'Heee haw!' },
+  { name: 'Betsy the Cow', 
+    catchphrase: 'Mooooooooo!' },
+  { name: 'Golly the Goose', 
+    catchphrase: 'Run pigs! Be free! I would if I could.' },
+  { name: 'Charlotte the Spider', 
+    catchphrase: 'Never hurry and never worry' }
+];
