042d621867685e49f931210ae8614c70a55a9cf5
austinbhan <austinbhan@outlook.com>
2022-06-07 12:58:39 -0700

    Trying to add background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <div id="header"><Header /></div>
-
+      <Main />
       <div id="footer"><Footer /></div>
     </div>
   );
