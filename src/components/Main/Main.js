6ddc815ff2cc54f60cbf9fcbdbcdafa3606fd93f
austinbhan <austinbhan@outlook.com>
2022-06-07 14:53:28 -0700

    Working background

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Main.css';
-import backgroundImg from './background.png';
+import backgroundImg from '../../background.png';
 
 export default function Main() {
   return (
-    <main style={{ backgroundImage: `url(${backgroundImg}).png` }}>
-        Moar Text Here
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      
     </main>
   );
 }
